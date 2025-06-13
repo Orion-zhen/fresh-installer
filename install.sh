15a33f01ca1e859c93dddbc4ca701b6deb1ec38c
Orion <128988082+Orion-zhen@users.noreply.github.com>
2025-06-13 22:34:46 +0800

    :fire: optional code

--- install.sh
+++ install.sh
@@ -11,22 +11,12 @@ git reset --hard origin/main
 git branch --set-upstream-to origin/main
 cd $HOME
 cp -rn $HOME/.config.bak/* $HOME/.config/
+rm -rf $HOME/.config.bak
 
 if [[ "$(uname)" == "Linux" ]]; then
-  if [[ "$1" == "--add-repo" ]]; then
-    sudo cp /etc/pacman.conf /etc/pacman.conf.bak
-    sudo curl -fsSL https://raw.githubusercontent.com/Orion-zhen/fresh-installer/refs/heads/main/pacman.conf >> /etc/pacman.conf
-  fi
   curl -fsSL https://raw.githubusercontent.com/Orion-zhen/fresh-installer/refs/heads/main/arch-pkgs.txt | xargs sudo pacman -Syu --noconfirm
-  sudo groupadd docker
-  sudo usermod -aG docker $USER
-  sudo systemctl enable --now docker.service docker.socket
-  sudo systemctl enable --now cronie.service
-  sudo systemctl enable --now sshd.service
-  sudo systemctl enable --now tailscaled.service
-  sudo systemctl enable --now cups.service cups.socket cups-browsed.service
 elif [[ "$(uname)" == "Darwin" ]]; then
   /bin/bash -c "$(curl -fsSL https://github.com/Homebrew/install/raw/master/install.sh)"
-  curl -fsSL https://raw.githubusercontent.com/Orion-zhen/fresh-installer/refs/heads/main/brew-pkg.txt > brew-pkg.txt
+  curl -fsSL https://raw.githubusercontent.com/Orion-zhen/fresh-installer/refs/heads/main/brew-pkg.txt >brew-pkg.txt
   brew bundle --file brew-pkg.txt
 fi
